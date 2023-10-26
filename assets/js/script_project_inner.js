1fdd792408eedc8fbc19d5063025fb919ac20760
DasunMadawa <dasunmadawa8864@gmail.com>
2023-10-26 22:57:49 +0530

    add projects inner page

--- assets/js/script_project_inner.js
+++ assets/js/script_project_inner.js
@@ -1,10 +1,21 @@
-let video = $("#video-player");
-video.play();
+let video = document.querySelector("#video-player");
+let play_button = $("#play-btn");
 
-$("#play-btn").on("click", function () {
-    $(this).css("display" , "none");
-    let video = $("#video-player");
-    video.controls = true;
-    console.log(video);
+play_button.on("click", function () {
+    try {
+        video.play();
+        video.setAttribute("controls" , "controls");
+        $(this).addClass("hide_play_btn");
+
+    } catch (e) {
+        console.log(e);
+    }
 
 });
+
+video.addEventListener("ended" , (event) => {
+    play_button.removeClass("hide_play_btn");
+    video.removeAttribute("controls");
+
+});
+
